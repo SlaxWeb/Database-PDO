3357505324d9b391b6a85cd5d5f5966d4bf86ade
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-02-16 14:46:13 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Service/Provider.php
+++ src/Service/Provider.php
@@ -33,7 +33,7 @@ class Provider implements \Pimple\ServiceProviderInterface
             return new \SlaxWeb\DatabasePDO\Library($container["pdo.service"], $container["queryBuilder.service"]);
         };
 
-        $container["pdo.service"] = function (Container $container) {
+        $container["pdo.service"] = function(Container $container) {
             $config = $container["config.service"]["database.connection"];
             $dsn = "{$config["driver"]}:dbname={$config["database"]};host={$config["hostname"]}";
             if (isset($config["port"]) === true && $config["port"] > 0) {
