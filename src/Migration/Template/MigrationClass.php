9b31ff5cd932e4fda5c82ad0f70b1c416cb06e0b
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-07-19 13:52:04 +0200

    add empty migration template

---
+++ src/Migration/Template/MigrationClass.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Migration;
+
+use SlaxWeb\DatabasePDO\Migration\BaseMigration;
+
+class MigrationClass extends BaseMigration
+{
+    /**
+     * @inheritDocs
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * @inheritDocs
+     */
+    public function down()
+    {
+    }
+}
