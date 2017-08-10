9ffa80bb29042cd14174b1a89fbaf36a69a2029f
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-08-10 08:58:35 +0200

    add migrations configuration file

---
+++ config/migration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Migration Config
+ *
+ * Database PDO sub-component migration configuration.
+ *
+ * @package   SlaxWeb\DatabasePDO
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2017 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.6
+ */
+/*
+ * Migration repository location
+ *
+ * Default: app/Migrations/
+ */
+$configuration["repository"] = __DIR__ . "/../Migrations/";
